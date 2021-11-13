716c96e6390d33a23eb0fbed29b3b7615d04494d
arenchilingaryan <arenchilingaryan@yandex.ru>
2021-11-13 06:46:45 -0800

    mf

---
+++ src/components/Auth.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const Auth = () => {
+  return (
+    <div className="div" style={{fontSize: '100px'}}>
+      AUTH
+    </div>
+  );
+};
+
+export default Auth;
